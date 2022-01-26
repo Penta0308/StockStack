36edb3524f52921a3bf2e2b6ccfe6a02acc3cb61
Penta0308 <penta@skmserver.tk>
2022-01-26 16:42:10 +0900

    single process

--- stocksheet/network/connection.py
+++ stocksheet/network/connection.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import TYPE_CHECKING, Optional
 
 import websockets
@@ -11,36 +12,22 @@ from stocksheet.network.packets import hello, internalerror, globaladmin, market
 
 if TYPE_CHECKING:
     from stocksheet.network.gateway import Gateway
-    from stocksheet.worker import WorkerProcess
 
 
 class ClientConnection:
     def __init__(self, gateway: 'Gateway', websocket: websockets.WebSocketServerProtocol):
         self.gateway = gateway
         self.websocket = websocket
-        self.uid, self._marketident = None, None
-        self.workerp: Optional['WorkerProcess'] = None
-        self.logger = Settings.get_logger()
-
-    def set_market(self, marketident):
-        m = self.gateway.workermanager.workers_get(marketident)
-        if m is None:
-            raise AttributeError('Undefined market')
-        self.workerp = m
-        self._marketident = marketident
-        return self._marketident
-
-    def get_market(self):
-        return self.workerp
+        self.uid = None
 
     async def send(self, p):
-        self.logger.debug(f"WebSocket Outbound > {p}")
+        logging.debug(f"WebSocket Outbound > {p}")
         return await self.websocket.send(p)
 
     async def run(self):
         async for message in self.websocket:
             try:
-                self.logger.debug(f"WebSocket Inbound  < {message}")
+                logging.debug(f"WebSocket Inbound  < {message}")
                 jo = json.loads(message)
                 pclass = PACKETS.packet_lookup(int(jo['op']))
                 if issubclass(pclass, PacketR):
--- stocksheet/network/gateway.py
+++ stocksheet/network/gateway.py
@@ -1,63 +1,31 @@
 import asyncio
 import json
-import ssl
-import typing
-from typing import TYPE_CHECKING
 
 import websockets
 
 from stocksheet.network.auth import Auth
-from stocksheet.network.packet import PACKET_OPCODE, PacketR, PacketT
-from stocksheet.network.packets import cmdlet, internalerror
+from stocksheet.network.connection import ClientConnection
+from stocksheet.network.packets import PACKETS
 
-if TYPE_CHECKING:
-    from stocksheet.worker import WorkerProcess
-
+# noinspection PyUnresolvedReferences
+from stocksheet.network.packets import globaladmin, hello, internalerror, marketadmin
 
 class Gateway:
-    def __init__(self, auth: Auth, host='127.0.0.1', port=5000, sslcert=None):
+    def __init__(self, auth: Auth, port, host='127.0.0.1'):
         self.__host = host
         self.__port = port
-        self.workerlut = dict()
         self.__eventloop = None
         self.__wsserver = None
-        self.__auth = auth
-        if sslcert is not None:
-            # noinspection PyTypeChecker
-            self.__ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            self.__ssl_context.load_cert_chain(certfile=sslcert["cert"], keyfile=sslcert["key"])
-        else:
-            self.__ssl_context = None
-
-    def workers_register(self, marketident: typing.Hashable, workerprocess: 'WorkerProcess'):
-        self.workerlut[marketident] = workerprocess
+        self.auth = auth
 
     def start(self):
         self.__eventloop = asyncio.get_event_loop()
         self.__wsserver = self.__eventloop.run_until_complete(
-            websockets.serve(self.handler_receive, self.__host, self.__port, ssl=self.__ssl_context))
+            websockets.serve(self.handler_receive, self.__host, self.__port))
         self.__eventloop.run_forever()
 
     def stop(self):
         self.__eventloop.stop()
 
     async def handler_receive(self, websocket: websockets.WebSocketServerProtocol):
-        token = await websocket.recv()
-
-        print(token)
-
-        uid = self.__auth.apikey_check(token)
-
-        if uid is None:
-            await websocket.close(1011, "Authentication Failed")
-
-        async for message in websocket:
-            try:
-                jo = json.loads(message)
-                pclass = PACKET_OPCODE(jo['op'])
-                if self.__auth.privilege_check(uid, pclass.REQUIRE_PRIVILEGE):
-                    await pclass.receive(websocket, jo['d'])
-                else:
-                    raise PermissionError()
-            except Exception as e:
-                await internalerror.InternalErrorResp.send(websocket, e)
+        await ClientConnection(self, websocket).run()
